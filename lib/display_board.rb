6edf11ca5d78857cb96f8a137c95d68ac3725ff0
Nathan Fedzen <nfedzen@hotmail.com>
2020-10-13 21:27:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = [" "," "," "," "," "," "," "," "," "]
-def display_board(rows = ["   /   /   "])
+def display_board(rows = ["   /   /   ","   /   /   ","   /   /   "])
   puts rows[0]
   puts "-----------"
   puts rows[1]
